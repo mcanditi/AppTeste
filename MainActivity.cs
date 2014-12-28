720ba0e33d48ff3d75236b62cd17bb089c69387c
mcanditi <matheuscandidosilva@hotmail.com>
2014-12-28 09:35:34 -0200

    Check-in inicial do módulo SalonMarketing

---
+++ MainActivity.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace SalonMarketing
+{
+	[Activity (Label = "SalonMarketing", MainLauncher = true, Icon = "@drawable/icon")]
+	public class MainActivity : Activity
+	{
+
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			SetContentView (Resource.Layout.Main);
+
+			//Button b = new Button ();
+
+			}
+		}
+	}
+
+
